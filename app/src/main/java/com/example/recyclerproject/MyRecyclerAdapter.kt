e938966e6f78b29d6f179d13b8b497babcd61303
Theo <theolin@immedia.co.za>
2021-09-27 13:35:20 +0200

    - coding formatting

--- app/src/main/java/com/example/recyclerproject/MyRecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerproject/MyRecyclerAdapter.kt
@@ -1,16 +1,15 @@
 package com.example.recyclerproject
 
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerproject.databinding.ListItemBinding
 
 
-class MyRecyclerAdapter  : RecyclerView.Adapter<MyViewHolder>() {
+class MyRecyclerAdapter : RecyclerView.Adapter<MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
-        return MyViewHolder(ListItemBinding.inflate(layoutInflater,parent,false))
+        return MyViewHolder(ListItemBinding.inflate(layoutInflater, parent, false))
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
@@ -22,6 +21,6 @@ class MyRecyclerAdapter  : RecyclerView.Adapter<MyViewHolder>() {
     }
 }
 
-class MyViewHolder(var binding:ListItemBinding) : RecyclerView.ViewHolder(binding.root){
+class MyViewHolder(var binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
 }
